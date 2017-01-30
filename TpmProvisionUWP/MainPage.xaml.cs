5e445341ab5eec4014dece37e560def875c02ce3
Artur Laksberg <arturl@microsoft.com>
2017-01-30 11:32:16 -0800

    TpmProvision demo

---
+++ TpmProvisionUWP/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Devices.Tpm;
+using Windows.UI.Xaml.Controls;
+
+namespace TpmProvisionUWP
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            TpmDevice myDevice = new TpmDevice(0);
+
+            // The following strings should be replaced with the actual information from Azure Portal or using the DeviceExplorer:
+
+            string primaryKey = "...";
+            string hostname = "iot-open-house-demo.azure-devices.net";
+            string deviceID = "humidity-sensor";
+
+            myDevice.Provision(primaryKey, hostname, deviceID);
+        }
+    }
+}
